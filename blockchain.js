412b804ceed6888f741cd1dc95261d1c7deaf3d1
broccolirob <rschn4911@gmail.com>
2019-06-25 16:13:24 -0700

    Added difficulty adjustment limit so that it never grows by more than one. Also added hex to binary method so that powerful computers don't lower target time.

--- blockchain.js
+++ blockchain.js
@@ -21,6 +21,10 @@ class Blockchain {
     for (let i = 1; i < chain.length; i++) {
       const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
       const actualLastHash = chain[i - 1].hash;
+      const lastDifficulty = chain[i - 1].difficulty;
+
+      if (lastHash !== actualLastHash) return false;
+
       const validatedHash = cryptoHash(
         timestamp,
         lastHash,
@@ -29,8 +33,8 @@ class Blockchain {
         difficulty
       );
 
-      if (lastHash !== actualLastHash) return false;
       if (hash !== validatedHash) return false;
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
     }
     return true;
   }
