130c8026db69968f3a69a9c18e375d993000774d
broccolirob <rschn4911@gmail.com>
2019-06-27 14:34:15 -0700

    Added wallet class and tests. Added elliptic library for generating and verifying key pairs and signatures.

---
+++ src/wallet/wallet.js
@@ -0,0 +1,17 @@
+const cryptoHash = require("../../utils/crypto-hash");
+const { STARTING_BALANCE } = require("../config");
+const { ec } = require("../../utils/elliptic-crypto");
+
+class Wallet {
+  constructor() {
+    this.balance = STARTING_BALANCE;
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode("hex");
+  }
+
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
+  }
+}
+
+module.exports = Wallet;
