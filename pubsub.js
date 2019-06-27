2c17f88d3c1a3c2b9ae59f31f04342a28ab1fd55
broccolirob <rschn4911@gmail.com>
2019-06-27 01:22:08 -0700

    Added ability to spin up multiple peers locally. Added initial sync chain when server starts.

--- pubsub.js
+++ pubsub.js
@@ -1,15 +1,17 @@
 const redis = require("redis");
 
 const CHANNELS = {
-  TEST: "TEST"
+  TEST: "TEST",
+  SCHRUTECHAIN: "SCHRUTECHAIN"
 };
 
 class PubSub {
-  constructor() {
+  constructor({ blockchain }) {
+    this.blockchain = blockchain;
     this.publisher = redis.createClient();
     this.subscriber = redis.createClient();
 
-    this.subscriber.subscribe(CHANNELS.TEST);
+    this.subscribeToChannels();
 
     this.subscriber.on("message", (channel, message) =>
       this.handleMessage(channel, message)
@@ -18,9 +20,28 @@ class PubSub {
 
   handleMessage(channel, message) {
     console.log(`Message received. Channel: ${channel}, Message: ${message}.`);
+    const parsedMessage = JSON.parse(message);
+    if (channel === CHANNELS.SCHRUTECHAIN) {
+      this.blockchain.replaceChain(parsedMessage);
+    }
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel => {
+      this.subscriber.subscribe(channel);
+    });
+  }
+
+  publish({ channel, message }) {
+    this.publisher.publish(channel, message);
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.SCHRUTECHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
   }
 }
 
-const testPubSub = new PubSub();
-
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, "foo"), 1000);
+module.exports = PubSub;
